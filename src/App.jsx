8210c406190668460630ee28d83115e76af4ab2a
KeanuFord <KeanuF2017@gmail.com>
2024-04-25 10:46:07 -0700

    Update app.jsx with nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
+import Nav from './components/Navbar/index.jsx';
 
 function App() {
 	// const [count, setCount] = useState(0);
 
 	return (
 		<>
+			<Nav />
 			<h1>Hello</h1>
 		</>
 	);
