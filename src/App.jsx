c445d60d284c282bc59e95176594e51da7bbd92f
Kyle <kyleyee522@gmail.com>
2024-04-25 10:53:14 -0700

    created routes for createrecipe

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 // import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
-import Nav from './components/Navbar/index.jsx';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/index.jsx';
+import CreateRecipe from './pages/CreateRecipe/index.jsx';
+import Home from './pages/Home/index.jsx';
 
 function App() {
 	// const [count, setCount] = useState(0);
 
 	return (
-		<>
-			<Nav />
-			<h1>Hello</h1>
-		</>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/createrecipe" element={<CreateRecipe />} />
+				<Route path="*" element={<h1>notFound</h1>} />
+			</Routes>
+		</Router>
 	);
 }
 
