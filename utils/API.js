2e6508e233b27756cdd072b91baa26e443a513e5
Kyle <kyleyee522@gmail.com>
2024-04-29 11:34:29 -0700

    successfully adds recipe to database

--- utils/API.js
+++ utils/API.js
@@ -1,45 +1,58 @@
-const URL_PREFIX="http://localhost:3001"
+const URL_PREFIX = 'http://localhost:3001';
 
 const API = {
+	// Signup
+	signup: (userObj) => {
+		//TODO: Verify backend route
+		return fetch(`${URL_PREFIX}/api/createUser`, {
+			method: 'POST',
+			body: JSON.stringify(userObj),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((res) => res.json());
+	},
 
-    // Signup
-    signup:userObj => {
-        //TODO: Verify backend route
-        return fetch(`${URL_PREFIX}/api/createUser`, {
-            method: "POST",
-            body: JSON.stringify(userObj),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json())
-    },
+	// Login
+	login: (userObj) => {
+		console.log('Logging in with credentials ');
+		console.log(userObj);
+		return fetch(`${URL_PREFIX}/api/users/login`, {
+			method: 'POST',
+			body: JSON.stringify(userObj),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}).then((res) => res.json());
+	},
 
-    // Login 
-    login:userObj => {
-        console.log("Logging in with credentials ");
-        console.log(userObj);
-        return fetch(`${URL_PREFIX}/api/users/login`, {
-            method: "POST",
-            body: JSON.stringify(userObj),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json())
-    },
+	//Check Token
+	checkToken: (token) => {
+		return fetch(`${URL_PREFIX}/api/users/checkToken`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		}).then((res) => res.json());
+	},
 
-    //Check Token
-    checkToken:token => {
-        return fetch(`${URL_PREFIX}/api/users/checkToken`, {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        }).then(res => res.json())
-    },
+	// Get one user
+	getOneUser: (userId) => {
+		return fetch(`${URL_PREFIX}/api/users/${userId}`).then((res) => res.json());
+	},
 
-    // Get one user
-    getOneUser:userId => {
-        return fetch(`${URL_PREFIX}/api/users/${userId}`).then(res => res.json())
-    }
+	// Create a recipe
+	createRecipe: (userObj, token) => {
+		{
+			return fetch(`${URL_PREFIX}/api/recipes`, {
+				method: 'POST',
+				body: JSON.stringify(userObj),
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			}).then((res) => res.json());
+		}
+	},
 };
 
 export default API;
